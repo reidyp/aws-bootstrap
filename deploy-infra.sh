98cc9dcfa9ca2d91e9a48c8a2e62ca58d0cd8b43
Paul Reidy <reidyp@localhost>
2020-10-25 12:57:42 -0400

    Add codebuild  / codedeploy

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -7,7 +7,25 @@ CLI_PROFILE=awsbootstrap
 
 EC2_INSTANCE_TYPE=t2.micro
 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \ 
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+
 # Deploy the cloud fomation template
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ===========" 
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \ 
+  --template-file setup.yml \ 
+  --no-fail-on-empty-changeset \ 
+  --capabilities CAPABILITY_NAMED_IAM \ 
+  --parameter-overrides \
+  CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 
 echo -e "\n\n================ Deploying main.yml ================="
 aws cloudformation deploy \
@@ -27,4 +45,5 @@ if [ $? -eq 0 ]; then
     --profile awsbootstrap \
     --query "Exports[?Name=='InstanceDNS'].Value" 
 fi
+
 
